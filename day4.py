165808e6ae2252ebf24eaab3e86bbc4030abf025
SaiAnvitha-k <78864147+SaiAnvitha-k@users.noreply.github.com>
2021-06-15 18:49:27 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,18 @@
+#Create three variables to same values of any integer and do divide , multiply and add
+a=b=c=40
+#a divided by 10
+print(a/10)
+#b multiply by 50
+print(b*50)
+#c add by 60
+print(c+60)
+
+#create string and replacing the third character
+string_var="world"
+print(string_var.replace("r","g"))
+
+#create two values of int and float data type & covert the vise versa
+a=10
+b=27.5
+print(float(a))
+print(int(b))
